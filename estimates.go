8c9c74be7672776402eb44ec36abd1f8da286ea1
Patrick Udomsirirat <upatrick@gmail.com>
2023-03-01 21:24:08 -0500

    Define Estimates Domain

---
+++ estimates.go
@@ -0,0 +1,21 @@
+package main
+
+import "time"
+
+type EstimateService interface {
+	CreateEstimate(CreateEstimateRequest) *Estimate
+}
+
+type Estimate struct {
+	Price             float64 `json:"price"`
+	Size              string  `json:"size"`
+	EstimatedDistance float64 `json:"estimated_distance"`
+}
+
+type CreateEstimateRequest struct {
+	Items            []Item     `json:"items"`
+	PickupLocation   Location   `json:"pickup_location"`
+	DeliveryLocation Location   `json:"delivery_location"`
+	PickupAfter      time.Time  `json:"pickup_after"`
+	DeliverBetween   TimeWindow `json:"deliver_between"`
+}
